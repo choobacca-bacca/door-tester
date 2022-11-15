312d61cf40d741352165d271f674c034e040465a
“MarcTestier” <marc.testier@gmail.com>
2022-11-15 12:10:43 +0000

    door controller for multithreading

--- door_controller_tester.py
+++ door_controller_tester.py
@@ -1,167 +1,202 @@
 from __future__ import annotations
 
-import requests
-import time
 import datetime
-import xmltodict
-
 import enum
-
-import requests
-import yaml
-from typing import Optional
-
-from awscrt import io, mqtt, auth, http
-from awsiot import mqtt_connection_builder
 import json
 import sys
+import threading
+import time
+from typing import Optional
 
-success_response = 0
-fail_response = 0
+import requests
+import xmltodict
+import yaml
+from awscrt import auth, http, io, mqtt
+from awsiot import mqtt_connection_builder
 
-def on_message_received(topic, payload, dup, qos, retain, **kwargs):
-    global response
-    response = json.loads(payload)
-    return response
 
-with open("config.yaml", 'r') as stream:
-    try:
-        config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+def status_publisher(mqtt_connect, rest_session):
+    DIPinOne = ""
+    DIPinTwo = ""
+    successOne = True
+    successTwo = True
 
-# Spin up resources
-event_loop_group = io.EventLoopGroup(1)
-host_resolver = io.DefaultHostResolver(event_loop_group)
-client_bootstrap = io.ClientBootstrap(
-    event_loop_group, host_resolver
-)
-mqtt_connection = mqtt_connection_builder.mtls_from_path(
-    endpoint=config["mqtt"]["api_endpoint"],
-    cert_filepath=config["mqtt"]["cert"],
-    pri_key_filepath=config["mqtt"]["key"],
-    client_bootstrap=client_bootstrap,
-    ca_filepath=config["mqtt"]["root_cert"],
-    client_id=config["mqtt"]["name"],
-    clean_session=False,
-    keep_alive_secs=6,
-)
-print(
-    "Connecting to {} with client ID '{}'...".format(
-        config["mqtt"]["api_endpoint"], config["mqtt"]["name"]
-    )
-)
-# Make the connect() call
-# Future.result() waits until a result is available
-try:
-    connect_future = mqtt_connection.connect()
-    connect_future.result()
-except:
-    print("Problem with connecting")
-    sys.exit(1)
+    i = 0
+    start = time.time()
+    interval = 1.0
+    while (i < 3600):
+        #    time.sleep(1
+        # print(str(time.time() - start))
+        if (time.time() - start > interval):
+            print(time.time() - start)
+            for door in config["doors"]:
+                try:
+                    response = rest_session.get(
+                        config["doors"][door]+"/digitalinput/0/value", timeout=(5, 30))
+                    successOne = True
+                    print(response.status_code)
+                    responseJSON = xmltodict.parse(response.content)
+                    DIPinOne = responseJSON["ADAM-6052"]["DI"]["ID"]
+                except Exception as err:
+                    print(f"{err}")
+                    successOne = False
+                    print("error for restful call 1")
 
-print("Connected!")
+                try:
+                    response = rest_session.get(
+                        config["doors"][door]+"/digitalinput/1/value", timeout=(5, 30))
+                    successTwo = True
+                    print(response.status_code)
+                    responseJSON = xmltodict.parse(response.content)
+                    DIPinTwo = responseJSON["ADAM-6052"]["DI"]["ID"]
+                except Exception as err:
+                    print(f"{err}")
+                    print("error for restful call 2")
 
-DIPinOne = ""
-DIPinTwo = ""
-successOne = True
-successTwo = True
+                door_mode = 0
 
-session = requests.Session()
-session.auth = ("root", "00000000")
-
-max = 31
-start = time.time()
-interval = 0.5
-print(start)
-i = 0
-
-while(i < 3600):
-#    time.sleep(1
-    print(str(time.time() - start))
-    if(time.time() - start > interval):
-        for door in config["doors"]:
-            try:
-                response = session.get(
-                    config["doors"][door]+"/digitalinput/0/value", timeout=5)
-                successOne = True
-                print(response.status_code)
-                responseJSON = xmltodict.parse(response.content)
-                DIPinOne = responseJSON["ADAM-6052"]["DI"]["ID"]
-            except Exception as err:
-                print(f"{err}")
-                successOne = False
-                print("error for restful call 1")
-
-            try:
-                response = session.get(
-                    config["doors"][door]+"/digitalinput/1/value", timeout=5)
-                successTwo = True
-                print(response.status_code)
-                responseJSON = xmltodict.parse(response.content)
-                DIPinTwo = responseJSON["ADAM-6052"]["DI"]["ID"]
-            except Exception as err:
-                print(f"{err}")
-                print("error for restful call 2")
-
-            door_mode = 0
-
-            if (successOne and successTwo):
-                if (DIPinTwo == "1"):
-                    door_mode = 0  # door is closed
-                elif (DIPinOne == "1"):
-                    door_mode = 2  # door is open
+                if (successOne and successTwo):
+                    if (DIPinTwo == "1"):
+                        door_mode = 0  # door is closed
+                    elif (DIPinOne == "1"):
+                        door_mode = 2  # door is open
+                    else:
+                        door_mode = 1  # door is moving
                 else:
-                    door_mode = 1  # door is moving
-            else:
-                door_mode = 3
+                    door_mode = 3
 
-            data = {
-                "door_name": door,
-                "current_mode": door_mode
-            }
-        start = time.time()
-        i += 1
+                data = {
+                    "door_name": door,
+                    "current_mode": door_mode
+                }
+            start = time.time()
+            i += 1
+            print(i)
 
-        # try:
-        #     publish_future, packet_id = mqtt_connection.publish(
-        #         topic=(config["mqtt"]["topic"] +
-        #                door + "/data"),
-        #         payload=json.dumps(data),
-        #         qos=mqtt.QoS.AT_LEAST_ONCE,
-        #     )
-        #     print("published to topic " + config["mqtt"]["topic"] +
-        #           door + "/data")
-        # except (KeyboardInterrupt, SystemExit):
-        #     print ("\nkeyboardinterrupt caught (again)")
-        #     print ("\n...Program Stopped Manually!")
-        #     raise
-        # except Exception as err:
-        #     print(f"{err}")
-        #     print("error for mqtt publish")
-        
-    print(i)
-    
-    # time.sleep(0.1)
+            try:
+                publish_future, packet_id = mqtt_connect.publish(
+                    topic=(config["mqtt"]["topic"] +
+                           door + "/data"),
+                    payload=json.dumps(data),
+                    qos=mqtt.QoS.AT_LEAST_ONCE,
+                )
+                print("published to topic " + config["mqtt"]["topic"] +
+                      door + "/data")
+            except (KeyboardInterrupt, SystemExit):
+                print("\nkeyboardinterrupt caught (again)")
+                print("\n...Program Stopped Manually!")
+                raise
+            except Exception as err:
+                print(f"{err}")
+                print("error for mqtt publish")
 
-    # remaining = max + start - time.time()
 
-    # if remaining <= 0:
-    #     break
+def command_subscriber(mqtt_connect, rest_session):
+    global mqtt_response
+    mqtt_response = {}
 
-        # try:
-        #     subscribe_future, packet_id = mqtt_connection.subscribe(
-        #         topic=config["mqtt"]["topic"] + door + "/command",
-        #         qos=mqtt.QoS.AT_LEAST_ONCE,
-        #         callback=on_message_received,
-        #     )
-        #     if (response["requested_mode"] == "1"):
-        #         door_request = {"DO0=" + response["requested_mode"]}
-        #         session.post(config["doors"][door]+"/digitaloutput/all/value", timeout=10, data="D01=0")
-        #         session.post(config["doors"][door]+"/digitaloutput/all/value", timeout=10, data=door_request)
+    def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+        global mqtt_response
+        mqtt_response = json.loads(payload)
+        return mqtt_response
 
-        # except Exception as err:
-        #     print(f"{err}")
-        #     print("error for mqtt publish")
+    while (True):
+        for door in config["doors"]:
+            time.sleep(1)
+            try:
+                subscribe_future, packet_id = mqtt_connect.subscribe(
+                    topic=config["mqtt"]["topic"] + door + "/command",
+                    qos=mqtt.QoS.AT_LEAST_ONCE,
+                    callback=on_message_received,
+                )
+                print(mqtt_response)
+                # res = json.loads(mqtt_response)
+                if (mqtt_response == {}):
+                    print("empty")
+                    rest_session.post(
+                        config["doors"][door]+"/digitaloutput/all/value", timeout=10, data="DO1=0")
+                elif (mqtt_response["requested_mode"] == "2"):
+                    rest_resp = rest_session.post(
+                        config["doors"][door]+"/digitaloutput/all/value", timeout=10, data="DO1=0")
+                    print(rest_resp)
+                    time.sleep(0.1)
+                    rest_resp = rest_session.post(
+                        config["doors"][door]+"/digitaloutput/all/value", timeout=10, data="DO1=1")
+                    print(rest_resp)
+                    print("door opened")
+                else:
+                    rest_resp = rest_session.post(
+                        config["doors"][door]+"/digitaloutput/all/value", timeout=10, data="DO1=0")
+                    print("door closed")
+                    print(rest_resp)
+                    mqtt_response = {}
+                    
+            except (KeyboardInterrupt, SystemExit):
+                print("\nkeyboardinterrupt caught (again)")
+                print("\n...Program Stopped Manually!")
+                raise
 
-    
+            except Exception as err:
+                print(f"{err}")
+                print("error for mqtt subscribe")
+
+
+if __name__ == "__main__":
+
+    with open("config.yaml", 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # Spin up resources
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(
+        event_loop_group, host_resolver
+    )
+
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        endpoint=config["mqtt"]["api_endpoint"],
+        cert_filepath=config["mqtt"]["cert"],
+        pri_key_filepath=config["mqtt"]["key"],
+        client_bootstrap=client_bootstrap,
+        ca_filepath=config["mqtt"]["root_cert"],
+        client_id=config["mqtt"]["name"],
+        clean_session=False,
+        keep_alive_secs=6,
+    )
+    print(
+        "Connecting to {} with client ID '{}'...".format(
+            config["mqtt"]["api_endpoint"], config["mqtt"]["name"]
+        )
+    )
+    # Make the connect() call
+    # Future.result() waits until a result is available
+    try:
+        connect_future = mqtt_connection.connect()
+        connect_future.result()
+    except:
+        print("Problem with connecting")
+        sys.exit(1)
+
+    print("Connected!")
+
+    session = requests.Session()
+    session.auth = ("root", "00000000")
+    # creating thread
+    t1 = threading.Thread(target=status_publisher, args=(mqtt_connection, session))
+    t2 = threading.Thread(target=command_subscriber, args=(mqtt_connection, session))
+
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
+
+    # wait until thread 1 is completely executed
+    t1.join()
+    # wait until thread 2 is completely executed
+    t2.join()
+
+    # both threads completely executed
+    print("Done!")
